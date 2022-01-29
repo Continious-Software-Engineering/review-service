1d976e07cdb9213b184b63639c1bb4f772ed4989
Robin Sadeghpour <r.sadegh.f@gmail.com>
2022-01-29 15:59:02 +0100

    Maven Dependency für spring grpc starter gefixt und dummy wert by getReview

--- src/main/java/reviewservice/ReviewSeriveImpl.java
+++ src/main/java/reviewservice/ReviewSeriveImpl.java
@@ -1,7 +1,43 @@
 package reviewservice;
 
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import review.ReviewServiceGrpc;
+import review.ReviewServiceOuterClass;
+import review.ReviewServiceOuterClass.Empty;
+import review.ReviewServiceOuterClass.AddReviewRequest;
+import review.ReviewServiceOuterClass.GetReviewsRequest;
+import review.ReviewServiceOuterClass.Review;
+import review.ReviewServiceOuterClass.UpdateReviewRequest;
+import review.ReviewServiceOuterClass.GetReviewRequest;
 
-
+@GrpcService
 public class ReviewSeriveImpl extends ReviewServiceGrpc.ReviewServiceImplBase {
+    public ReviewSeriveImpl() {
+        super();
+    }
+
+    @Override
+    public void addReview(AddReviewRequest request, StreamObserver<Empty> responseObserver) {
+        super.addReview(request, responseObserver);
+    }
+
+    @Override
+    public void getReviews(GetReviewsRequest request, StreamObserver<Review> responseObserver) {
+        super.getReviews(request, responseObserver);
+    }
+
+    @Override
+    public void getReview(GetReviewRequest request, StreamObserver<Review> responseObserver) {
+        Review response = Review.newBuilder()
+                .setReviewId("123")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateReview(UpdateReviewRequest request, StreamObserver<Empty> responseObserver) {
+        super.updateReview(request, responseObserver);
+    }
 }

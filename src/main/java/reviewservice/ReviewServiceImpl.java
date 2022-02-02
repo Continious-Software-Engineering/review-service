20f593faf33580d791f4b881b8e9a2f621aa038e
robinsadeghpour <RobinRojin2>
2022-02-02 20:03:56 +0100

    Added example log statements and added lombok maven dependecy.

--- src/main/java/reviewservice/ReviewServiceImpl.java
+++ src/main/java/reviewservice/ReviewServiceImpl.java
@@ -1,17 +1,26 @@
 package reviewservice;
 
 import io.grpc.stub.StreamObserver;
+import lombok.extern.java.Log;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
 import review.ReviewServiceGrpc;
 import review.ReviewServiceOuterClass.*;
 
-import static java.lang.String.valueOf;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import static java.lang.String.valueOf;
+import static java.util.logging.Level.INFO;
+import static jdk.nashorn.internal.objects.NativeMath.log;
+
+@Log
 @GrpcService
 public class ReviewServiceImpl extends ReviewServiceGrpc.ReviewServiceImplBase {
 
     @Override
     public void addReview(AddReviewRequest request, StreamObserver<Empty> responseObserver) {
+        log(INFO, "ADD REVIEW");
         Empty response = Empty.newBuilder().build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
@@ -19,6 +28,7 @@ public class ReviewServiceImpl extends ReviewServiceGrpc.ReviewServiceImplBase {
 
     @Override
     public void getReviews(GetReviewsRequest request, StreamObserver<Review> responseObserver) {
+        log(INFO, "GET REVIEWS");
         for (int i = 0; i < 5; i++) {
             Review response = Review.newBuilder()
                     .setReviewId(valueOf(i))
@@ -33,6 +43,7 @@ public class ReviewServiceImpl extends ReviewServiceGrpc.ReviewServiceImplBase {
 
     @Override
     public void getReview(GetReviewRequest request, StreamObserver<Review> responseObserver) {
+        log(INFO, "GET REVIEW");
         Review response = Review.newBuilder()
                 .setReviewId("review_id")
                 .setUserId("user_id")
@@ -45,6 +56,7 @@ public class ReviewServiceImpl extends ReviewServiceGrpc.ReviewServiceImplBase {
 
     @Override
     public void updateReview(UpdateReviewRequest request, StreamObserver<Empty> responseObserver) {
+        log(INFO, "UPDATE REVIEW");
         Empty response = Empty.newBuilder().build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();

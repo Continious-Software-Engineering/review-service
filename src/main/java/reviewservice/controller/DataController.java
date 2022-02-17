d2b6cce9b584e4a250f29ea322e701900518876c
Tijana Milentijevic <83136272+tijanamilentijevic@users.noreply.github.com>
2022-02-17 16:49:16 +0100

    #28 MongoDB Setup (#29)

---
+++ src/main/java/reviewservice/controller/DataController.java
@@ -0,0 +1,54 @@
+package reviewservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
+import reviewservice.model.Review;
+import reviewservice.repository.ReviewRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@EnableJpaRepositories("reviewservice.repository")
+@Component
+public class DataController {
+    private ReviewRepository reviewRepository;
+
+    public List<Review> getAllReviews(String productId) {
+        List<Review> reviews = new ArrayList<>();
+        try {
+            reviews = reviewRepository.findAll().stream()
+                    .filter(review -> productId.equals(review.getProductId()))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        try {
+            reviewRepository.save(review);
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+    }
+
+    public void deleteReview(String reviewId) {
+        try {
+            reviewRepository.deleteById(reviewId);
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+    }
+
+    @Autowired
+    public void setReviewRepository(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+}
